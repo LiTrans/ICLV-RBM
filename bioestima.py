b556e4eb16fa4e87492bf5b107094e67b6636b8e
melvin <melvin.wong@polymtl.ca>
2017-06-30 14:30:57 -0400

    estimation files (30/6/17)

---
+++ bioestima.py
@@ -0,0 +1,149 @@
+########################################
+#
+# @file bioestima.py
+# @originalAuthor: Michel Bierlaire, EPFL
+# @modified: Melvin Wong, Ryerson University
+#
+#######################################
+
+from biogeme import *
+from headers import *
+from loglikelihood import *
+from statistics import *
+
+import numpy as np
+
+#Parameters to be estimated
+# Arguments:
+#   - 1  Name for report; Typically, the same as the variable.
+#   - 2  Starting value.
+#   - 3  Lower bound.
+#   - 4  Upper bound.
+#   - 5  0: estimate the parameter, 1: keep it fixed.
+#
+
+labels = np.asarray(['_BUS', '_CARRENTAL', '_CAR', '_PLANE', '_TRH', '_TRAIN'])
+
+# alternative specific parameters
+ASC = [Beta('ASC'+str(label),0,-10,10,np.floor(i/5).astype(np.int),'ASC'+str(label)) for i, label in enumerate(labels)]
+
+B_DRVLICENS = [Beta('B_DRVLICENS'+str(label),0,-10,10,np.floor(i/5).astype(np.int),'B_DRVLICENS'+str(label)) for i, label in enumerate(labels)]
+
+B_PBLCTRST = [Beta('B_PBLCTRST'+str(label),0,-10,10,np.floor(i/5).astype(np.int),'B_PBLCTRST'+str(label)) for i, label in enumerate(labels)]
+#
+# B_AG1825 = [Beta('B_AG1825'+str(label),0,-10,10,np.floor(i/5).astype(np.int),'B_AG1825'+str(label)) for i, label in enumerate(labels)]
+#
+# B_AG2545 = [Beta('B_AG2545'+str(label),0,-10,10,np.floor(i/5).astype(np.int),'B_AG2545'+str(label)) for i, label in enumerate(labels)]
+#
+# B_AG4565 = [Beta('B_AG4565'+str(label),0,-10,10,np.floor(i/5).astype(np.int),'B_AG4565'+str(label)) for i, label in enumerate(labels)]
+#
+# B_AG65M = [Beta('B_AG65M'+str(label),0,-10,10,np.floor(i/5).astype(np.int),'B_AG65M'+str(label)) for i, label in enumerate(labels)]
+#
+# B_MALE = [Beta('B_MALE'+str(label),0,-10,10,np.floor(i/5).astype(np.int),'B_MALE'+str(label)) for i, label in enumerate(labels)]
+#
+# B_FULLTIME = [Beta('B_FULLTIME'+str(label),0,-10,10,np.floor(i/5).astype(np.int),'B_FULLTIME'+str(label)) for i, label in enumerate(labels)]
+#
+# B_PRTTIME = [Beta('B_PRTTIME'+str(label),0,-10,10,np.floor(i/5).astype(np.int),'B_PRTTIME'+str(label)) for i, label in enumerate(labels)]
+#
+# B_UNEMPLYD = [Beta('B_UNEMPLYD'+str(label),0,-10,10,np.floor(i/5).astype(np.int),'B_UNEMPLYD'+str(label)) for i, label in enumerate(labels)]
+#
+# B_EDU_HIGHSCHL = [Beta('B_EDU_HIGHSCHL'+str(label),0,-10,10,np.floor(i/5).astype(np.int),'B_EDU_HIGHSCHL'+str(label)) for i, label in enumerate(labels)]
+#
+# B_EDU_BSC = [Beta('B_EDU_BSC'+str(label),0,-10,10,np.floor(i/5).astype(np.int),'B_EDU_BSC'+str(label)) for i, label in enumerate(labels)]
+#
+# B_EDU_MSCPHD = [Beta('B_EDU_MSCPHD'+str(label),0,-10,10,np.floor(i/5).astype(np.int),'B_EDU_MSCPHD'+str(label)) for i, label in enumerate(labels)]
+
+
+# generic parameters
+B_COST = Beta('B_COST',0,-10,10,0,'B_COST')
+B_TIME = Beta('B_TIME',0,-10,10,0,'B_TIME')
+B_RELI = Beta('B_RELI',0,-10,10,0,'B_RELI')
+B_COST_S = Beta('B_COST_S',0,-10,10,0,'B_COST_S')
+B_TIME_S = Beta('B_TIME_S',0,-10,10,0,'B_TIME_S')
+B_RELI_S = Beta('B_RELI_S',0,-10,10,0,'B_RELI_S')
+
+BIOGEME_OBJECT.DRAWS = {
+    'B_COST_RND': 'NORMAL',
+    'B_TIME_RND': 'NORMAL',
+    'B_RELI_RND': 'NORMAL'}
+
+B_COST_RND = B_COST + B_COST_S * bioDraws('B_COST_RND')
+B_TIME_RND = B_TIME + B_TIME_S * bioDraws('B_TIME_RND')
+B_RELI_RND = B_RELI + B_RELI_S * bioDraws('B_RELI_RND')
+
+altspec_params = np.asarray([B_DRVLICENS, B_PBLCTRST])#, B_AG1825, B_AG2545, B_AG4565, B_AG65M, B_MALE, B_FULLTIME, B_PRTTIME, B_UNEMPLYD, B_EDU_HIGHSCHL, B_EDU_BSC, B_EDU_MSCPHD])
+gen_params = np.asarray([B_COST_RND, B_TIME_RND, B_RELI_RND])
+
+# generic variables
+gen_vars = np.asarray([DrvLicens, PblcTrst])#, Ag1825, Ag2545, Ag4565, Ag65M, Male, Fulltime, PrtTime, Unemplyd, Edu_Highschl, Edu_BSc, Edu_MscPhD])
+
+# alternative specific variables
+bus = [Bus_Cost/100., Bus_TT/100., BusRelib/100.]
+carrental = [CarRental_Cost/100., CarRental_TT/100., CarRentalRelib/100.]
+car = [Car_Cost/100., Car_TT/100., CarRelib/100.]
+plane = [Plane_Cost/100., Plane_TT/100., PlaneRelib/100.]
+trh = [TrH_Cost/100., TrH_TT/100., TrHRelib/100.]
+train = [Train_Cost/100., Train_TT/100., TrainRelib/100.]
+
+altspec_vars = np.asarray([bus, carrental, car, plane, trh, train])
+
+# Utility functions
+# For numerical reasons, it is good practice to scale the data to
+# that the values of the parameters are around 1.0.
+# A previous estimation with the unscaled data has generated
+# parameters around -0.01 for both cost and time. Therefore, time and
+# cost are multipled by 0.01.
+
+V1 = ASC[0] + np.dot(gen_params, altspec_vars[0]) + np.dot(gen_vars, altspec_params[:,0])
+V2 = ASC[1] + np.dot(gen_params, altspec_vars[1]) + np.dot(gen_vars, altspec_params[:,1])
+V3 = ASC[2] + np.dot(gen_params, altspec_vars[2]) + np.dot(gen_vars, altspec_params[:,2])
+V4 = ASC[3] + np.dot(gen_params, altspec_vars[3]) + np.dot(gen_vars, altspec_params[:,3])
+V5 = ASC[4] + np.dot(gen_params, altspec_vars[4]) + np.dot(gen_vars, altspec_params[:,4])
+V6 = ASC[5] + np.dot(gen_params, altspec_vars[5]) + np.dot(gen_vars, altspec_params[:,5])
+
+# Associate utility functions with the numbering of alternatives
+V = {1: V1, 2: V2, 3: V3, 4: V4, 5: V5, 6: V6}
+
+# Associate the availability conditions with the alternatives
+av = {1: AV_Bus,
+      2: AV_CarRental,
+      3: AV_Car,
+      4: AV_Plane,
+      5: AV_TrH,
+      6: AV_Train}
+
+# The choice model is a logit, with availability conditions
+# logprob = bioLogLogit(V, av, New_SP_Choice)
+# # Defines an itertor on the data
+# rowIterator('obsIter')
+# # DEfine the likelihood function for the estimation
+# BIOGEME_OBJECT.ESTIMATE = Sum(logprob,'obsIter')
+
+prob = bioLogit(V, av, New_SP_Choice)
+l = mixedloglikelihood(prob)
+rowIterator('obsIter')
+# Likelihood function
+BIOGEME_OBJECT.ESTIMATE = Sum(l,'obsIter')
+
+# All observations verifying the following expression will not be
+# considered for estimation
+# The modeler here has developed the model only for work trips.
+# Observations such that the dependent variable CHOICE is 0 are also removed.
+# exclude = (( PURPOSE != 1 ) * (  PURPOSE   !=  3  ) + ( CHOICE == 0 )) > 0
+
+# BIOGEME_OBJECT.EXCLUDE = exclude
+
+# Statistics
+
+nullLoglikelihood(av,'obsIter')
+choiceSet = [1, 2, 3, 4, 5, 6]
+cteLoglikelihood(choiceSet, New_SP_Choice, 'obsIter')
+availabilityStatistics(av, 'obsIter')
+
+BIOGEME_OBJECT.PARAMETERS['optimizationAlgorithm'] = "BIO"
+BIOGEME_OBJECT.PARAMETERS['numberOfThreads'] = "4"
+BIOGEME_OBJECT.PARAMETERS['NbrOfDraws'] = "25"
+
+BIOGEME_OBJECT.FORMULAS['Bus utility'] = V1
+BIOGEME_OBJECT.FORMULAS['CarRental utility'] = V2
+BIOGEME_OBJECT.FORMULAS['Car utility'] = V3
---
+++ run_mxl.py
@@ -0,0 +1,211 @@
+import numpy as np
+import pandas as pd
+import pickle, os, sys, timeit
+import theano
+import theano.tensor as T
+from theano import shared, function
+from theano.tensor.shared_randomstreams import RandomStreams
+
+from dataWrangling import *
+from MixedLogit import *
+from optimizers import *
+
+floatX = theano.config.floatX
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+pd.options.display.max_rows = 999
+
+def main():
+    """ Discrete choice model estimation with theano
+        Under normal circumstances, no modification
+        necessary
+
+    Setup
+    -----
+    step 1: Load variables from csv file
+    step 2: Define hyperparameters used in the computation
+    step 3: define symbolic Theano tensors
+    step 4: build model and define cost function
+    step 5: define gradient calculation algorithm
+    step 6: define Theano symbolic functions
+    """
+    # compile and import dataset from csv#
+    dataset = Preprocessing('US_SP_Restructured.csv')
+    dataset_x_ng, dataset_x_g, dataset_y, avail, data_ind = dataset.data()
+    data_x_ng = shared(np.asarray(dataset_x_ng, dtype=floatX), borrow=True)
+    data_x_g = shared(np.asarray(dataset_x_g, dtype=floatX), borrow=True)
+    data_y = T.cast(shared(np.asarray(dataset_y-1, dtype=floatX), borrow=True), 'int32')
+    data_av = shared(np.asarray(avail, dtype=floatX), borrow=True)
+
+    sz_n = dataset_x_g.shape[0]  # number of samples
+    sz_m = dataset_x_ng.shape[2] # number of non-generic variables
+    sz_k = dataset_x_g.shape[1]  # number of generic variables
+    sz_i = dataset_x_ng.shape[1] # number of alternatives
+
+    # random draws
+    sz_draw = 1000
+    srng = RandomStreams(1234)
+    rng = srng.normal((sz_n, sz_draw, sz_m))
+
+    # model hyperparameters
+    sz_minibatch = sz_n
+    learning_rate = 1e-3
+    momentum = 0.9
+
+    # symbolic theano tensors
+    x_ng = T.tensor3('data_x_ng')
+    x_g = T.matrix('data_x_g')
+    y = T.ivector('data_y')
+    av = T.matrix('data_av')
+
+    index = T.lscalar('index')
+
+    draws = T.tensor3('normal_draws')
+
+    # construct model
+    model = MixedLogit(sz_i, av, input=[x_ng, x_g], n_in=[(sz_m), (sz_k, sz_i)], draws=draws)
+    cost = -model.loglikelihood(y)
+
+    # calculate the gradients wrt to the loss function
+    grads = T.grad(cost=cost, wrt=model.params)
+    optimizer = rmsprop(model.params)
+    updates = optimizer.updates(model.params, grads, learning_rate, momentum)
+
+    fn_checkdraw = function([],
+        outputs=model.draws,
+        givens={
+        draws: rng},)
+
+    # hessian function
+    fn_hessian = function([],
+        outputs=T.hessian(cost=cost, wrt=model.params),
+        givens={
+        x_ng: data_x_ng,
+        x_g: data_x_g,
+        y: data_y,
+        av: data_av,
+        draws: rng},)
+
+    # null loglikelihood function
+    fn_null = function([],
+        outputs=-model.loglikelihood(y),
+        givens={
+        x_ng: data_x_ng,
+        x_g: data_x_g,
+        y: data_y,
+        av: data_av,
+        draws: rng},)
+
+    # compile the theano functions
+    fn_estimate = function(
+        name='estimate',
+        inputs=[index],
+        outputs=[model.loglikelihood(y), model.errors(y)],
+        updates=updates,
+        givens={
+            x_ng: data_x_ng[index*sz_minibatch : T.min(((index+1)*sz_minibatch,sz_n))],
+            x_g: data_x_g[index*sz_minibatch : T.min(((index+1)*sz_minibatch,sz_n))],
+            y: data_y[index*sz_minibatch : T.min(((index+1)*sz_minibatch,sz_n))],
+            av: data_av[index*sz_minibatch : T.min(((index+1)*sz_minibatch,sz_n))],
+            draws: rng[index*sz_minibatch : T.min(((index+1)*sz_minibatch,sz_n))]},
+        allow_input_downcast=True,
+        on_unused_input='ignore',)
+
+    """ Main estimation process loop """
+    print('Begin estimation...')
+    # process loop parameters
+    epoch = 0
+    sz_epoches = 2500
+    sz_batches = sz_n // sz_minibatch
+    done_looping = False
+    patience = 100
+    patience_inc = 10
+    best_loglikelihood = -np.inf
+    null_Loglikelihood = -fn_null()
+    start_time = timeit.default_timer()
+
+    while epoch < sz_epoches and done_looping is False:
+        epoch_error = []
+        epoch_loglikelihood = []
+        for i in range(sz_batches):
+            (batch_loglikelihood, batch_error) = fn_estimate(i)
+            epoch_error.append(batch_error)
+            epoch_loglikelihood.append(batch_loglikelihood)
+
+        this_loglikelihood = np.mean(epoch_loglikelihood)
+        print('@ iteration %d loglikelihood: %.3f'
+            % (epoch, this_loglikelihood))
+
+        if this_loglikelihood > best_loglikelihood:
+            if this_loglikelihood > 0.995 * best_loglikelihood:
+                patience += patience_inc
+            best_loglikelihood = this_loglikelihood
+            with open('best_model.pkl', 'wb') as f: pickle.dump(model, f)
+
+        if epoch > patience:
+            done_looping = True
+
+        epoch +=1
+
+    final_Loglikelihood = best_loglikelihood
+    rho_square = 1.-(final_Loglikelihood/null_Loglikelihood)
+    end_time = timeit.default_timer()
+
+    """ Analytics and model statistics """
+    print('@iteration %d, run time %.3f '
+        % (epoch, end_time-start_time))
+    print('Null Loglikelihood: %.3f'
+        % null_Loglikelihood)
+    print('Final Loglikelihood: %.3f'
+        % final_Loglikelihood)
+    print('rho square %.3f'
+        % rho_square)
+    print('draws %d'
+        % sz_draw)
+
+    with open('best_model.pkl', 'rb') as f: best_model=pickle.load(f)
+    print('... solving Hessians')
+    h = np.hstack([np.diagonal(mat) for mat in fn_hessian()])
+    run_analytics(best_model, h)
+
+def run_analytics(model, hessians):
+    stderr =2/np.sqrt(hessians)
+    betas = np.concatenate([param.get_value() for param in model.params], axis=0)
+    t_stat = betas/stderr
+    data = np.vstack((betas, stderr, t_stat)).T
+    columns = ['betas', 'serr', 't_stat']
+
+    # print dataFrame
+    paramNames = []
+    choices = ['Bus', 'CarRental', 'Car', 'Plane', 'TrH', 'Train']
+    ASCs = ['ASC_Bus', 'ASC_CarRental', 'ASC_Car', 'ASC_Plane', 'ASC_TrH', 'ASC_Train']
+    nongenericNames =  ['cost', 'tt', 'relib','cost_s', 'tt_s', 'relib_s']
+    genericNames = [
+         'DrvLicens', 'PblcTrst', \
+    #    'Ag1825', 'Ag2545', 'Ag4565', 'Ag65M', 'Male', 'Fulltime', 'PrtTime', 'Unemplyd', 'Edu_Highschl', 'Edu_BSc', 'Edu_MscPhD', 'HH_Veh0', 'HH_Veh1', \
+    # 	'HH_Veh2M', 'HH_Adult1', 'HH_Adult2', 'HH_Adult3M', 'HH_Chld0', 'HH_Chld1', 'HH_Chld2M', \
+    # 	'HH_Inc020K', 'HH_Inc2060K', 'HH_Inc60KM', 'HH_Sngl', 'HH_SnglParent', \
+    # 	'HH_AllAddults', 'HH_Nuclear', 'P_Chld', 'O_MTL_US_max', 'O_Odr_US_max', 'D_Bstn_max', \
+    # 	'D_NYC_max', 'D_Maine_max', 'Tp_Onewy_max', 'Tp_2way_max', 'Tp_h06_max', 'Tp_h69_max', \
+    # 	'Tp_h915_max', 'Tp_h1519_max', 'Tp_h1924_max', 'Tp_h1524_max', 'Tp_Y2016_max', 'Tp_Y2017_max', \
+    # 	'Tp_Wntr_max', 'Tp_Sprng_max', 'Tp_Sumr_max', 'Tp_Fall_max', \
+    # 	'Tp_CarDrv_max', 'Tp_CarPsngr_max', 'Tp_CarShrRnt_max', 'Tp_Train_max', 'Tp_Bus_max', \
+    # 	'Tp_Plane_max', 'Tp_ModOdr_max', 'Tp_WrkSkl_max', 'Tp_Leisr_max', 'Tp_Shpng_max', \
+    # 	'Tp_ActOdr_max',  'Tp_NHotel1_max', \
+    # 	'Tp_NHotel2_max', 'Tp_NHotel3M_max', 'Tp_FreqMonthlMulti_max', \
+    # 	'Tp_FreqYearMulti_max'
+    ]
+
+    for ASC in ASCs:
+        paramNames.append(ASC)
+    for name in nongenericNames:
+        paramNames.append(name)
+    for name in genericNames:
+        for choice in choices:
+            paramNames.append(name+'_'+choice)
+
+    df = pd.DataFrame(data, paramNames, columns)
+    print(df)
+
+
+if __name__ == '__main__':
+    main()
